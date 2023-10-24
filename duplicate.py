cc9a95ade78c0fe10bf42a35460f51398fb25454
Kenny Ketelaar <kenny@ketelaar.tech>
2023-10-24 23:27:35 +0200

    Update correctness for duplicate
    Also added some comments

--- duplicate.py
+++ duplicate.py
@@ -3,8 +3,10 @@ import sys
 
 duplication_factor = int(sys.argv[1])
 
-df = pd.read_csv("agaricus-lepiota.csv")
+# read the .data as a csv, since it is in that format anyway
+df = pd.read_csv("agaricus-lepiota.data")
 
+# concate the dataframe duplication_factor times with itself
 new_df = pd.concat([df] * duplication_factor)
 
 new_df.to_csv(f"mushrooms-{duplication_factor}.csv", index=False)
